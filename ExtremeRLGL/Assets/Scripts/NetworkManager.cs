f6da439c02fa5cdaf90079f2a619ddecd74ef3a8
Sharon <caisharo@cs.washington.edu>
2022-01-29 03:39:32 -0800

    Added option to create and join specific rooms in multiplayer. Needed to add oculus.software.overlay_keyboard to AndroidManifest.xml file for overlay keyboard for text input.

--- ExtremeRLGL/Assets/Scripts/NetworkManager.cs
+++ ExtremeRLGL/Assets/Scripts/NetworkManager.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
     public GameObject connectButton;
-    public GameObject successText;
-    public GameObject failText;
+    public GameObject connectedText;
+
+    public GameObject roomUI;
+    public GameObject createUI;
+    public GameObject joinUI;
+    public GameObject joinErrorText;
+    public GameObject joinInput;
+    public TextMeshProUGUI createdRoomCode;
 
     [SerializeField] private byte maxPlayersPerRoom = 10;
     string gameVersion = "1";
@@ -33,42 +40,110 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     public void ConnectToServer()
     {
+        Debug.Log("Trying to connect to server...");
         PhotonNetwork.ConnectUsingSettings();
         PhotonNetwork.GameVersion = gameVersion;
-        Debug.Log("Trying to connect to server...");
+    }
+    public void QuickMatch()
+    {
+        // The first we try to do is to join a potential existing room. 
+        // If there is, good, else, we'll be called back with OnJoinRandomFailed()
+        PhotonNetwork.JoinRandomRoom();
+
+        // Once enough players are in scene, we can start game? 
+        // Or some sort of manual start?
     }
 
-    // currently not using - hardcoded room 1
-    public void InitializeRoom()
+    public void CreateRoom()
     {
-        Debug.Log("Initializing room.");
+        // Generate a random 4 letter room name
+        string roomName = "";
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < 4; i ++)
+        {
+            roomName += chars[Random.Range(0, chars.Length)];
+        }
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = maxPlayersPerRoom;
-        roomOptions.IsVisible = true;
-        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = false;  // not possible to join randomly
+        roomOptions.IsOpen = true;  // let others join
 
-        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
+        // Set text
+        createdRoomCode.text = "<b>Room Code:</b> " + roomName;
+
+        // Create and join room
+        Debug.Log("Creating and joining room " + roomName);
+        PhotonNetwork.CreateRoom(roomName, roomOptions, TypedLobby.Default);
+    }
+
+    public void JoinRoom()
+    {
+        string roomName = joinInput.GetComponent<TMP_InputField>().text;
+        Debug.Log("Trying to join room " + roomName);
+        PhotonNetwork.JoinRoom(roomName.ToUpper());
+    }
+
+    public void LeaveRoom()
+    {
+        Debug.Log("Leaving room.");
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("Starting game.");
+        PhotonNetwork.CurrentRoom.IsOpen = false;
+        // TODO: Let person who created room start game (probably switch scene)
+        // PhotonNetwork.LoadLevel(levelName);
     }
 
     public override void OnConnectedToMaster()
     {
-        base.OnConnectedToMaster();
         Debug.Log("Connected to master server.");
+        PhotonNetwork.JoinLobby();
+
+        // Hiding connect button UI only if actually connected.
+        // In case of no internet, player will not be able to succesfully connect (but they won't crash either).
+        // Probably want to handle better later.
         connectButton.SetActive(false);
-        successText.SetActive(true);
-
-        StartCoroutine(RemoveAfterSeconds(3, successText));
-
-        // PhotonNetwork.JoinLobby();
-
-        // The first we try to do is to join a potential existing room. 
-        // If there is, good, else, we'll be called back with OnJoinRandomFailed()
-        PhotonNetwork.JoinRandomRoom();
+        roomUI.SetActive(true);
+        connectedText.SetActive(true);
+        StartCoroutine(RemoveAfterSeconds(3, connectedText));
     }
     IEnumerator RemoveAfterSeconds(int seconds, GameObject obj)
     {
         yield return new WaitForSeconds(seconds);
         obj.SetActive(false);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        // Create another room if failed (e.g. current name already used)
+        Debug.Log("Failed to create room.");
+        CreateRoom();
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join room: " + message);
+        joinErrorText.SetActive(true);
+        StartCoroutine(RemoveAfterSeconds(3, joinErrorText));
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a room.");
+        joinErrorText.SetActive(false);
+        joinUI.SetActive(false);
+    }
+
+    public override void OnLeftRoom()
+    {
+        Debug.Log("Left room.");
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A new player joined the room.");
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
@@ -89,13 +164,4 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         Debug.LogWarningFormat("OnDisconnected() was called by PUN with reason {0}", cause);
     }
 
-    public override void OnJoinedRoom()
-    {
-        Debug.Log("Joined a room.");
-    }
-
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        Debug.Log("A new player joined the room.");
-    }
 }
