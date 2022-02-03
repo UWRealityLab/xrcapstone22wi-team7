e9a6dd0cce20186ff3ea18dce895adc7338c2c31
tngo2001 <tngo2001@uw.edu>
2022-02-03 12:53:53 -0800

    Implemented the running movement so that players move forward when they swing their arms
    The direction they go depends on which direction the left controller is facing

---
+++ ExtremeRLGL/Assets/Scripts/RunningMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningMovement : MonoBehaviour
+{
+    // Headset, controller, and container game objects
+    public GameObject LeftHand;
+    public GameObject RightHand;
+    public GameObject MainCamera;
+    public GameObject RunningContainer;
+
+    // Initial position coordinates
+    private Vector3 initLeftPos;
+    private Vector3 initRightPos;
+    private Vector3 initPlayerPos;
+
+    // Current position coordinates
+    private Vector3 currLeftPos;
+    private Vector3 currRightPos;
+    private Vector3 currPlayerPos;
+
+    // Speed variable to determine how far the player moves forward
+    public float speed = 80;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get initial position coordinates
+        initPlayerPos = transform.position;
+        initLeftPos = LeftHand.transform.position;
+        initRightPos = RightHand.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the container depending on the rotation of the left controller
+        RunningContainer.transform.eulerAngles = new Vector3(0, LeftHand.transform.eulerAngles.y, 0);
+
+        // Get current position coordinates
+        currLeftPos = LeftHand.transform.position;
+        currRightPos = RightHand.transform.position;
+        currPlayerPos = transform.position;
+
+        // Get distance between initial and current position coordinates
+        float playerDist = Vector3.Distance(initPlayerPos, currPlayerPos);
+        float leftDist = Vector3.Distance(initLeftPos, currLeftPos) - playerDist;
+        float rightDist = Vector3.Distance(initRightPos, currRightPos) - playerDist;
+  
+        // Calculate how much the player moves forward
+        transform.position += RunningContainer.transform.forward * (leftDist + rightDist) * speed * Time.deltaTime;
+
+        // Set current position coordinates as initial position coordinates
+        initLeftPos = currLeftPos;
+        initRightPos = currRightPos;
+        initPlayerPos = currPlayerPos;
+    }
+}
